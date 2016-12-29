1f71ee6ff8e588f0b21076f8dbb0d8059cf43cad
saravanancse <saravanakumar.ncse@gmail.com>
2016-12-29 14:44:09 +0530

    Add files via upload

---
+++ int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    int n;
+    char c[10];
+    printf("enter the characetr:");
+    scanf("%s",c);
+    n=atoi(c);
+    printf("%d",n);
+    return (0);
+}
